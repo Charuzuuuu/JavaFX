9f1bb04d9489e7799f6f0f989f074d6afcc11c24
Charles Darwin Hudar <144643288+Charuzuuuu@users.noreply.github.com>
2024-03-11 17:18:23 +0800

    Create HelloController.java

---
+++ HelloController.java
@@ -0,0 +1,23 @@
+package com.example.firstjavafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+public class Welcome extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root =  FXMLLoader.load(getClass().getResource("hello.fxml"));
+
+        Scene scene = new Scene(root, 300, 275);
+
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    
+}
