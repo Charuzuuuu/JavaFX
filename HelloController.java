56d246351259cd66be18c1fa5ca1525c1e9281f6
Charles Darwin Hudar <144643288+Charuzuuuu@users.noreply.github.com>
2024-03-11 17:21:12 +0800

    Create HelloController.java

---
+++ HelloController.java
@@ -0,0 +1,41 @@
+package com.example.firstjavafx;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
+public class HelloController {
+    @FXML
+    private AnchorPane pnMain;
+
+    @FXML
+    private AnchorPane pnAnchor;
+
+    @FXML
+    private VBox pnLogin;
+    @FXML
+    private VBox pnHome;
+
+    @FXML
+    protected void onLogInClick() throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("welcome.fxml"));
+        pnMain.getScene().getStylesheets().add(getClass().getResource("hello.css").toExternalForm());
+        pnMain.getChildren().remove(pnLogin);
+        pnMain.getChildren().add(root);
+    }
+
+    @FXML
+    protected void onLogOutClick() throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("hello.fxml"));
+        pnAnchor.getScene().getStylesheets().add(getClass().getResource("hello.css").toExternalForm());
+        pnAnchor.getChildren().remove(pnHome);
+        pnAnchor.getChildren().add(root);
+    }
+
+}
